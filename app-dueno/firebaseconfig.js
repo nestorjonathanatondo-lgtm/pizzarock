f4bf0872ab614605dd91539c0d08cfaef1f91e04
J&MDesign <nestorjonathanatondo@gmail.com>
2026-08-30 14:13:22 -0700

    Add Firebase configuration file

---
+++ app-dueno/firebaseconfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "TU_API_KEY_AQUI",
+  authDomain: "the-pizza-rock-saltillo.firebaseapp.com",
+  databaseURL: "https://the-pizza-rock-saltillo-default-rtdb.firebaseio.com",
+  projectId: "the-pizza-rock-saltillo",
+  storageBucket: "the-pizza-rock-saltillo.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abc"
+};
