94466320499550a1cbb9b724bc934176a782a7ed
J&MDesign <nestorjonathanatondo@gmail.com>
2026-08-30 14:25:42 -0700

    Update Firebase configuration with new credentials

--- app-repartidor/firebaseconfig.js
+++ app-repartidor/firebaseconfig.js
@@ -1,9 +1,17 @@
-export const firebaseConfig = {
-  apiKey: "TU_API_KEY_AQUI",
-  authDomain: "the-pizza-rock-saltillo.firebaseapp.com",
-  databaseURL: "https://the-pizza-rock-saltillo-default-rtdb.firebaseio.com",
-  projectId: "the-pizza-rock-saltillo",
-  storageBucket: "the-pizza-rock-saltillo.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abc"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA0ra49d5BlxcPC-vkfe82xZBD8l3X1ZxU",
+  authDomain: "pizza-rock-3154b.firebaseapp.com",
+  projectId: "pizza-rock-3154b",
+  storageBucket: "pizza-rock-3154b.firebasestorage.app",
+  messagingSenderId: "159293503780",
+  appId: "1:159293503780:web:e75bfa8a64ddf3cfaf1bb1"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
