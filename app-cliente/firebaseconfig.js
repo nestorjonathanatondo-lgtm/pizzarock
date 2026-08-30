eabc6f09494646ba3c0019306d7bd3d51b6734bc
J&MDesign <nestorjonathanatondo@gmail.com>
2026-08-30 14:24:00 -0700

    Update Firebase configuration for new project
    Updated Firebase configuration with new API keys and project details.

---
+++ app-cliente/firebaseconfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA0ra49d5BlxcPC-vkfe82xZBD8l3X1ZxU",
+  authDomain: "pizza-rock-3154b.firebaseapp.com",
+  projectId: "pizza-rock-3154b",
+  storageBucket: "pizza-rock-3154b.firebasestorage.app",
+  messagingSenderId: "159293503780",
+  appId: "1:159293503780:web:e75bfa8a64ddf3cfaf1bb1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);9:web:abc"
+};
